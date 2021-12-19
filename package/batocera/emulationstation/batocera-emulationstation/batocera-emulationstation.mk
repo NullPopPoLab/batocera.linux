b1cb9040e6812f1b22a83c3ae7e0f197ee12b6eb
sylph@reset.jp <NullPopPo>
2021-12-19 14:23:41 +0000

    patch済状態からのビルドを試す

--- package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
+++ package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BATOCERA_EMULATIONSTATION_VERSION = 4fa6f46d9c095fec935c9e4f7c97a34470c2ecc6
+BATOCERA_EMULATIONSTATION_VERSION = 2856254608cfa35cb3e506948455d5bcfc4a7012
 BATOCERA_EMULATIONSTATION_SITE = https://github.com/NullPopPoLab/batocera-emulationstation
 BATOCERA_EMULATIONSTATION_SITE_METHOD = git
 BATOCERA_EMULATIONSTATION_LICENSE = MIT

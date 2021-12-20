fd09d256373e4caceb03c4e4ffffadb0744c6dd1
sylph@reset.jp <NullPopPo>
2021-12-20 09:24:10 +0000

    patch済状態からのビルドを試す

--- package/batocera/emulators/retroarch/retroarch/retroarch.mk
+++ package/batocera/emulators/retroarch/retroarch/retroarch.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 # Version.: Release on Dec 05, 2021
-RETROARCH_VERSION = ce39402a49c874d15ba10b08b54eac8959dd2554
-RETROARCH_SITE = https://github.com/NullPopPoLab/RetroArch
+RETROARCH_VERSION = 15e282fc4a84d94b05b6db3f71d62a1defcfc009
+RETROARCH_SITE = https://github.com/NullPopPoLab/RetroArch.git
 RETROARCH_SITE_METHOD = git
 RETROARCH_LICENSE = GPLv3+
 RETROARCH_DEPENDENCIES = host-pkgconf dejavu retroarch-assets flac

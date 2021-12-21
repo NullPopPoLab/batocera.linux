da46e49ea4d80de5b7ab8cea3086972d6e4aadd3
sylph@reset.jp <NullPopPo>
2021-12-21 13:15:06 +0000

    retroarch: 追加ボタンのcfg保存対応

--- package/batocera/emulators/retroarch/retroarch/retroarch.mk
+++ package/batocera/emulators/retroarch/retroarch/retroarch.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version.: Release on Dec 05, 2021
-RETROARCH_VERSION = 9df0a87f209be0ca33f23033c04a4374f36672e5
+RETROARCH_VERSION = e344868ea23267902922d94c414e5efe63eedcab
 RETROARCH_SITE = https://github.com/NullPopPoLab/RetroArch
 RETROARCH_SITE_METHOD = git
 RETROARCH_LICENSE = GPLv3+

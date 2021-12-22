3533bfb1c72806e56d3778b7ecb1ad37610ac1cc
sylph@reset.jp <NullPopPo>
2021-12-22 06:29:37 +0000

    retroarch: 変数定義忘れ。

--- package/batocera/emulators/retroarch/retroarch/retroarch.mk
+++ package/batocera/emulators/retroarch/retroarch/retroarch.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version.: Release on Dec 05, 2021
-RETROARCH_VERSION = 3529473a1babc52155c9ba8aa12de5190a0d2681
+RETROARCH_VERSION = fee0c9be1e2ae18a3a77cd8c6ff4db4fd65ee97f
 RETROARCH_SITE = https://github.com/NullPopPoLab/RetroArch
 RETROARCH_SITE_METHOD = git
 RETROARCH_LICENSE = GPLv3+

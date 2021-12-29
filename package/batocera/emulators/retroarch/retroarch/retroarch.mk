903584afe4673af43330b4b240b8dd586df071dc
sylph@reset.jp <NullPopPo>
2021-12-29 18:58:02 +0000

    retroarch: config_save_autoconf_profile() ではinputsディレクトリ存在が前提

--- package/batocera/emulators/retroarch/retroarch/retroarch.mk
+++ package/batocera/emulators/retroarch/retroarch/retroarch.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 # Version.: Release on Dec 05, 2021
-RETROARCH_VERSION = v1.9.14
-RETROARCH_SITE = $(call github,libretro,RetroArch,$(RETROARCH_VERSION))
+RETROARCH_VERSION = 67d85d48639fc30b0df01780bd3ca36af81cfa09
+RETROARCH_SITE = https://github.com/NullPopPoLab/RetroArch
+RETROARCH_SITE_METHOD = git
 RETROARCH_LICENSE = GPLv3+
 RETROARCH_DEPENDENCIES = host-pkgconf dejavu retroarch-assets flac
 # install in staging for debugging (gdb)

f9a7dcab100264270643b129b3be21d1464b0b02
sylph@reset.jp <NullPopPo>
2022-01-02 14:28:24 +0000

    picodrive: retro_pico_map 添字とkey IDは無関係とする

--- package/batocera/emulators/retroarch/libretro/libretro-picodrive/libretro-picodrive.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-picodrive/libretro-picodrive.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version.: Commits on Nov 14, 2021
-LIBRETRO_PICODRIVE_VERSION = 395af7d31fe4ab6b616ef206fad1bac7155e22e6
+LIBRETRO_PICODRIVE_VERSION = bff8b96edd7895623fee5ad274a59ce13b4a65c9
 LIBRETRO_PICODRIVE_SITE = https://github.com/NullPopPoLab/picodrive
 LIBRETRO_PICODRIVE_SITE_METHOD=git
 LIBRETRO_PICODRIVE_GIT_SUBMODULES=YES

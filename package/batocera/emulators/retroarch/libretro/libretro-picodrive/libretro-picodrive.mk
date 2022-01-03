d46cc996e763ca93a592536021d2596d7340614b
sylph@reset.jp <NullPopPo>
2022-01-03 01:16:18 +0000

    retro_pico_map retro側bitmask確認

--- package/batocera/emulators/retroarch/libretro/libretro-picodrive/libretro-picodrive.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-picodrive/libretro-picodrive.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version.: Commits on Nov 14, 2021
-LIBRETRO_PICODRIVE_VERSION = bff8b96edd7895623fee5ad274a59ce13b4a65c9
+LIBRETRO_PICODRIVE_VERSION = 47cc9d8579cca0ecfe8fe2321ebf3421f725478e
 LIBRETRO_PICODRIVE_SITE = https://github.com/NullPopPoLab/picodrive
 LIBRETRO_PICODRIVE_SITE_METHOD=git
 LIBRETRO_PICODRIVE_GIT_SUBMODULES=YES

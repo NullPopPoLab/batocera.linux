ae9659628d9fccfad153cd844649ee035665aa5c
sylph@reset.jp <NullPopPo>
2022-01-06 10:41:34 +0000

    genesispluggx-wide,beetle-saturn,kronos,yabasanshiro: 16ボタン対応

--- package/batocera/emulators/retroarch/retroarch/retroarch.mk
+++ package/batocera/emulators/retroarch/retroarch/retroarch.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version.: Release on Dec 05, 2021
-RETROARCH_VERSION = 9b3023b17e1869740bd3349ad66b495a7bb15540
+RETROARCH_VERSION = 19ca749b1bbad8edec3d7ff5a114318143fe8209
 RETROARCH_SITE = https://github.com/NullPopPoLab/RetroArch
 RETROARCH_SITE_METHOD = git
 RETROARCH_LICENSE = GPLv3+

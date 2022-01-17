aa123c6c17b2b4a999cf19a6ca9155aa6543208a
sylph@reset.jp <NullPopPo>
2022-01-17 14:44:07 +0000

    リポジトリ間違い

--- package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
+++ package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 BATOCERA_EMULATIONSTATION_VERSION = e6f9fb076ded733a69368c256da9093f90db9946
-BATOCERA_EMULATIONSTATION_SITE = https://github.com/NullPopPoLab/EmulationStation
+BATOCERA_EMULATIONSTATION_SITE = https://github.com/NullPopPoLab/batocera-emulationstation
 BATOCERA_EMULATIONSTATION_SITE_METHOD = git
 BATOCERA_EMULATIONSTATION_LICENSE = MIT
 BATOCERA_EMULATIONSTATION_GIT_SUBMODULES = YES

9c4951e1d8cb7c8897b10b85fe61a9d3ed95fa1c
sylph@reset.jp <NullPopPo>
2022-01-22 09:05:59 +0000

    pc88: 余計なopen抑制

--- package/batocera/emulators/retroarch/libretro/libretro-pc88/libretro-pc88.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-pc88/libretro-pc88.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version.: Commits on Dec 10, 2021
-LIBRETRO_PC88_VERSION = 46847908727d074cf54d049fb089b44fc8113ee4
+LIBRETRO_PC88_VERSION = 8c33cc411db0ffbd549854bbfd28d505e47c074a
 LIBRETRO_PC88_SITE = https://github.com/NullPopPoLab/quasi88-libretro
 LIBRETRO_PC88_SITE_METHOD = git
 LIBRETRO_PC88_LICENSE = BSD 3-Clause

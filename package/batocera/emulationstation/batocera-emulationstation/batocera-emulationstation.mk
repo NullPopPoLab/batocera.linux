15cc6ee53a008c49cfe58ae6bbd18a7e8b93f63f
sylph@reset.jp <NullPopPo>
2022-01-31 00:20:10 +0000

    先祖返り。

--- package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
+++ package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BATOCERA_EMULATIONSTATION_VERSION = df329570ff8be55f7ee09cbf06746fce4ea4502d
+BATOCERA_EMULATIONSTATION_VERSION = f188d27d5997706d2e57adc2f7eb1c8dedfcdc11
 BATOCERA_EMULATIONSTATION_SITE = https://github.com/NullPopPoLab/batocera-emulationstation
 BATOCERA_EMULATIONSTATION_SITE_METHOD = git
 BATOCERA_EMULATIONSTATION_LICENSE = MIT

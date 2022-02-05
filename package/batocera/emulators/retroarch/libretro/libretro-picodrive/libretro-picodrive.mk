8826ddc6f850e973c710b9a8e30384405c4fef03
sylph@reset.jp <NullPopPo>
2022-02-05 10:33:16 +0000

    close忘れ。

--- package/batocera/emulators/retroarch/libretro/libretro-picodrive/libretro-picodrive.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-picodrive/libretro-picodrive.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version.: Commits on Nov 14, 2021
-LIBRETRO_PICODRIVE_VERSION = f923ae064e0a0db96fc99817033c82d0a5cd4871
+LIBRETRO_PICODRIVE_VERSION = 5ad201e9fcd53e0bda28c957c3a505eddfa74737
 LIBRETRO_PICODRIVE_SITE = https://github.com/NullPopPoLab/picodrive
 LIBRETRO_PICODRIVE_SITE_METHOD=git
 LIBRETRO_PICODRIVE_GIT_SUBMODULES=YES

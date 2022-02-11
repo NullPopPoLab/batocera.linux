bda7f0886071ca482ec4560559e62d3e6e0ef6ed
sylph@reset.jp <NullPopPo>
2022-02-11 10:18:27 +0000

    grid画像をthumbnailにする

--- package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
+++ package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 # Version.: Commits on Nov 30, 2021
-ES_THEME_CARBON_VERSION = 94c6322799fc543abf5205292002d1aedce89ffc
-ES_THEME_CARBON_SITE = $(call github,fabricecaruso,es-theme-carbon,$(ES_THEME_CARBON_VERSION))
+ES_THEME_CARBON_VERSION = e57058c9bfaf09924af012d26ae0b069b52af989
+ES_THEME_CARBON_SITE = https://github.com/NullPopPoLab/es-theme-carbon.git
+ES_THEME_CARBON_SITE_METHOD = git
 
 define ES_THEME_CARBON_INSTALL_TARGET_CMDS
     mkdir -p $(TARGET_DIR)/usr/share/emulationstation/themes/es-theme-carbon

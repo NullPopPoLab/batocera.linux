0a60d1d2b899325e872b0eb96b1e3ff6a5ae21d6
sylph@reset.jp <NullPopPo>
2022-02-11 05:16:21 +0000

    クラス名抜け。

--- package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
+++ package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BATOCERA_EMULATIONSTATION_VERSION = 631454ca0ea2146fcfc541fdb3714262fc096b74
+BATOCERA_EMULATIONSTATION_VERSION = 9ce863fb9c9514aeb2a0b226aacce38eceb0994d
 BATOCERA_EMULATIONSTATION_SITE = https://github.com/NullPopPoLab/batocera-emulationstation
 BATOCERA_EMULATIONSTATION_SITE_METHOD = git
 BATOCERA_EMULATIONSTATION_LICENSE = MIT

4f238fa74329846fe3186759580917c0e7eb9ced
sylph@reset.jp <NullPopPo>
2022-02-13 22:41:19 +0000

    公式ソース落ちてこないので別ルート開通を試す

--- package/batocera/emulators/wine-x86/wine-x86.mk
+++ package/batocera/emulators/wine-x86/wine-x86.mk
@@ -5,9 +5,9 @@
 ################################################################################
 
 # version 5.18 means binary from 5.18 version (or the last built if the version is not yet out)
-WINE_X86_VERSION = $(BATOCERA_SYSTEM_VERSION)
-WINE_X86_SOURCE = wine-x86-$(WINE_X86_VERSION).tar.lzma
-WINE_X86_SITE = https://github.com/batocera-linux/wine-x86/releases/download/$(WINE_X86_VERSION)
+WINE_X86_VERSION = b700985d57ba5d4c0cd301ff5ecd5237684bb274
+WINE_X86_SITE = https://github.com/NullPopPoLab/batocera-wine-x86
+WINE_X86_SITE_METHOD = git
 
 define WINE_X86_EXTRACT_CMDS
 	mkdir -p $(@D)/target && cd $(@D)/target && tar xf $(DL_DIR)/$(WINE_X86_DL_SUBDIR)/$(WINE_X86_SOURCE)

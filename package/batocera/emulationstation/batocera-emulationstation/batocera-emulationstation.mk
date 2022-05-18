27553ba87612e969dc7028bf92b7130022ef35b5
sylph@reset.jp <NullPopPo>
2022-05-18 16:25:34 +0000

    違うディレクトリの同名ファイルに対しADVANCED GAME OPTIONSの使い分け可

--- package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
+++ package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BATOCERA_EMULATIONSTATION_VERSION = d1fd3f90a8ce9b3f52fe4ff2e54252c4063553d8
+BATOCERA_EMULATIONSTATION_VERSION = d9cecb60590d06cf592e572517ae3267318871e0
 BATOCERA_EMULATIONSTATION_SITE = https://github.com/NullPopPoLab/batocera-emulationstation
 BATOCERA_EMULATIONSTATION_SITE_METHOD = git
 BATOCERA_EMULATIONSTATION_LICENSE = MIT

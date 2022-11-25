ed24257b8d22041cfd0b628146746c08d2abb007
sylph@reset.jp <NullPopPo>
2022-11-25 13:45:26 +0000

    wine: 言語選択対応 configgen: 環境変数のオーバーライド対応

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/wine/wineGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/wine/wineGenerator.py
@@ -9,16 +9,19 @@ import controllersConfig
 class WineGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, gameResolution):
+        cmd=None
         if system.name == "windows_installers":
             commandArray = ["batocera-wine", "windows", "install", rom]
-            return Command.Command(array=commandArray)
+            cmd=Command.Command(array=commandArray)
         elif system.name == "windows":
             commandArray = ["batocera-wine", "windows", "play", rom]
-            return Command.Command(array=commandArray,env={
+            cmd=Command.Command(array=commandArray,env={
                 "SDL_GAMECONTROLLERCONFIG": controllersConfig.generateSdlGameControllerConfig(playersControllers),
             })
-
-        raise Exception("invalid system " + system.name)
+        else: raise Exception("invalid system " + system.name)
+        if 'lang' in system.config and system.config['lang'] != '':
+            cmd.env['LANG']=cmd.env['LC_ALL']=system.config['lang']+'.UTF-8'
+        return cmd
 
     def getMouseMode(self, config):
         if "force_mouse" in config and config["force_mouse"] == "0":

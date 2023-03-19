e66f0b79b18f10e54ae9ec92e5a73101eb4cf8e3
sylph@reset.jp <NullPopPo>
2023-03-19 12:53:14 +0000

    7b8c45a270454f05e2dbf3beeb4afcf817db65da is missing. (this package is removed in future version) next best, use newest.

--- package/batocera/network/rtl8821cu/rtl8821cu.mk
+++ package/batocera/network/rtl8821cu/rtl8821cu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RTL8821CU_VERSION = 7b8c45a270454f05e2dbf3beeb4afcf817db65da
+RTL8821CU_VERSION = 80781de376007ecd778dfb2bef05533e3a6cc7c6
 RTL8821CU_SITE = $(call github,morrownr,8821cu-20210916,$(RTL8821CU_VERSION))
 RTL8821CU_LICENSE = GPL-2.0
 RTL8821CU_LICENSE_FILES = LICENSE

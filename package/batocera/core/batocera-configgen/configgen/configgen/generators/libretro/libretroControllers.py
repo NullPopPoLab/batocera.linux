b991d4cdb73eca81264116f3cc9d93059381cc30
NullPopPo <sylph@reset.jp>
2023-10-31 04:04:57 +0000

    some cores are supported twin swappable disks

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/libretro/libretroControllers.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/libretro/libretroControllers.py
@@ -26,6 +26,8 @@ hatstoname = {'1': 'up', '2': 'right', '4': 'down', '8': 'left'}
 # (NullPopPoCustom) Additional m3u support: PicoDrive / X1 / PPSSPP
 coreWithSwapSupport = {'hatari', 'cap32', 'bluemsx', 'dosbox_pure', 'flycast', 'np2kai', 'puae', 'px68k', 'vice_x64', 'vice_x64sc', 'vice_xplus4', 'vice_x128', 'pcsx_rearmed', 'duckstation', 'mednafen_psx', 'beetle-saturn', 'kronos', 'genesisplusgx', 'picodrive', 'pce', 'pce_fast', 'fceumm', 'nestopia', 'quasi88', 'x1', 'pcsx2', 'pcfx', 'ppsspp'};
 systemToSwapDisable = {'amigacd32', 'amigacdtv', 'naomi', 'atomiswave', 'megadrive', 'mastersystem', 'gamegear'}
+# (NullPopPoCustom) 2 disk drives support : MSX / PC88 / PC98 / X1 / X68000
+secondaryDiskDriveSupport = {'bluemsx','quasi88','np2kai','px68k','x1'}
 
 # Write a configuration for a specified controller
 # Warning, function used by amiberry because it reads the same retroarch formatting
@@ -49,7 +51,7 @@ def writeControllersConfig(retroconfig, system, controllers, lightgun):
                             '7':  'rewind',              '8':  'hold_fast_forward', '9': 'screenshot', \
                             '10': 'disk_prev',           '11': 'disk_next',         '12': 'disk_eject_toggle', \
                             '13': 'shader_prev',         '14': 'shader_next',       '15': 'ai_service', \
-                            '16': 'menu_toggle'}
+                            '16': 'menu_toggle',         '17': 'disk2_eject_toggle'}
     cleanControllerConfig(retroconfig, controllers, retroarchFullSpecial)
 
     # No menu in non full uimode

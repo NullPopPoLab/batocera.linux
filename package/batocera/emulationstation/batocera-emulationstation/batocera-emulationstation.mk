8d52cce4643fcf2cd05032b740f3d61db5c57945
NullPopPo <sylph@reset.jp>
2023-11-09 01:35:56 +0000

    EmulationStation: custom feat upgraded

--- package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
+++ package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Last update: Commits on Sept 27, 2023
-BATOCERA_EMULATIONSTATION_VERSION = 59c45112987e79edbdf39ad80c90250fc6fa4380
+BATOCERA_EMULATIONSTATION_VERSION = 6c8c273abb980bd6a65720f42514bac8ea71814e
 BATOCERA_EMULATIONSTATION_SITE = https://github.com/NullPopPoLab/batocera-emulationstation.git
 BATOCERA_EMULATIONSTATION_SITE_METHOD = git
 BATOCERA_EMULATIONSTATION_LICENSE = MIT

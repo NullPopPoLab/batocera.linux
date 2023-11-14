b7a57cc51d8a973a2188fd5c5ae2d4f1a07a4a03
NullPopPo <sylph@reset.jp>
2023-11-14 01:39:21 +0000

    configgen: can eject 2nd disk drive by hotkey

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/libretro/libretroControllers.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/libretro/libretroControllers.py
@@ -26,6 +26,8 @@ hatstoname = {'1': 'up', '2': 'right', '4': 'down', '8': 'left'}
 # Systems with internal mapping : PC88 / FDS | No multi-disc support : opera / yabasanshiro | No m3u support : PicoDrive
 coreWithSwapSupport = {'hatari', 'cap32', 'bluemsx', 'dosbox_pure', 'flycast', 'np2kai', 'puae', 'puae2021', 'px68k', 'vice_x64', 'vice_x64sc', 'vice_xscpu64', 'vice_xplus4', 'vice_x128', 'pcsx_rearmed', 'duckstation', 'mednafen_psx', 'beetle-saturn', 'kronos', 'genesisplusgx', 'pcsx2', 'pcfx'};
 systemToSwapDisable = {'amigacd32', 'amigacdtv', 'naomi', 'atomiswave', 'megadrive', 'mastersystem', 'gamegear'}
+# (NullPopPoCustom) 2 disk drives support : MSX / PC88 / PC98 / X1 / X68000
+secondaryDiskDriveSupport = {'bluemsx','quasi88','np2kai','px68k','x1'}
 
 # Write a configuration for a specified controller
 # Warning, function used by amiberry because it reads the same retroarch formatting
@@ -42,6 +44,9 @@ def writeControllersConfig(retroconfig, system, controllers, lightgun):
         retroarchspecials["l2"] =     "disk_prev"
         retroarchspecials["r2"] =     "disk_next"
         retroarchspecials["l3"] =     "screenshot"
+
+    if (system.config['core'] in secondaryDiskDriveSupport) and (system.name not in systemToSwapDisable):
+        retroarchspecials["r3"] = "disk2_eject_toggle"
 
     # Full special features list to disable
     retroarchFullSpecial = {'1':  'state_slot_increase', '2':  'load_state',        '3': 'save_state', \

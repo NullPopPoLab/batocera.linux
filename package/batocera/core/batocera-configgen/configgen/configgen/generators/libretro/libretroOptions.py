e57379654f31bd0ff1c495a525a50c5497388f15
NullPopPo <sylph@reset.jp>
2025-05-11 05:46:54 +0000

    libretro-kronos: add COMPUTE SHADERS (but not work)

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/libretro/libretroOptions.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/libretro/libretroOptions.py
@@ -2233,6 +2233,11 @@ def generateCoreSettings(coreSettings, system, rom, guns):
             coreSettings.save('kronos_videocoretype', '"' + system.config['kronos_videocoretype'] + '"')
         else:
             coreSettings.save('kronos_videocoretype', '"opengl"')
+        # Compute Shaders
+        if system.isOptSet('kronos_use_cs') and system.config['kronos_use_cs'] != 'disabled':
+            coreSettings.save('kronos_use_cs', '"' + system.config['kronos_use_cs'] + '"')
+        else:
+            coreSettings.save('kronos_use_cs', '"disabled"')
         # Polygon mode
         if system.isOptSet('kronos_polygon_mode') and system.config['kronos_polygon_mode'] != 'perspective_correction':
             coreSettings.save('kronos_polygon_mode', '"' + system.config['kronos_polygon_mode'] + '"')

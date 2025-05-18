1c9e9e713a7cee585318e62cfc17ce0235ff53aa
NullPopPo <sylph@reset.jp>
2025-05-18 04:56:02 +0000

    retroarch: RETROK_* redefinition for NC38

--- package/batocera/emulators/retroarch/retroarch/retroarch.mk
+++ package/batocera/emulators/retroarch/retroarch/retroarch.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version.: Commits on Jul 13, 2023
-RETROARCH_VERSION = 0a64b0749fd5a4b1f5fba015fd265facbcc59716
+RETROARCH_VERSION = 5bd38f71fa92a2c68e2bc38c237ba87f5ff5305f
 RETROARCH_SITE = https://github.com/NullPopPoLab/RetroArch.git
 RETROARCH_SITE_METHOD = git
 RETROARCH_LICENSE = GPLv3+

cc433d2a8b0efb36afc4284450a8fcd3ea402d22
NullPopPo <sylph@reset.jp>
2025-05-23 15:30:18 +0000

    libretro-pc88 upto NCA38

--- package/batocera/emulators/retroarch/libretro/libretro-pc88/libretro-pc88.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-pc88/libretro-pc88.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version: Commits on Jan 03, 2023
-LIBRETRO_PC88_VERSION = 8ecbcdc00a2b6e74c5cadaa20fa3c10a909b7e9a
+LIBRETRO_PC88_VERSION = ca537273565f42916d988c04aa0d9acfea3e1c44
 LIBRETRO_PC88_SITE = https://github.com/NullPopPoLab/quasi88-libretro
 LIBRETRO_PC88_SITE_METHOD = git
 LIBRETRO_PC88_LICENSE = BSD 3-Clause
